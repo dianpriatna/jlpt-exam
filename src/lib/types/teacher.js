f71926536d2f3b88ddeffd2a5aa4228d78b59efa
dianpriatna <dian.priatn@gmail.com>
2026-07-12 23:22:09 +0700

    feat: add Teacher type definition

---
+++ src/lib/types/teacher.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} Teacher
+ *
+ * @property {string} id
+ * @property {string} nip
+ * @property {string} name
+ * @property {string} email
+ * @property {boolean} active
+ */
