51dad4ef5cff83386c7c391509891919826bde36
dianpriatna <dian.priatn@gmail.com>
2026-07-12 22:49:17 +0700

    feat: add Question type definition

---
+++ src/lib/types/question.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Question
+ *
+ * @property {number} id
+ * @property {'N5'|'N4'|'N3'} level
+ * @property {string} category
+ * @property {string} question
+ * @property {string[]} choices
+ * @property {number} answer
+ */
