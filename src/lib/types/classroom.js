ebe429543ce4843e9558e2076ce7641177370c54
dianpriatna <dian.priatn@gmail.com>
2026-07-12 23:24:49 +0700

    feat: add Classroom type definition

---
+++ src/lib/types/classroom.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} Classroom
+ *
+ * @property {string} id
+ * @property {string} name
+ * @property {string} teacherId
+ * @property {string[]} studentIds
+ * @property {boolean} active
+ */
