19aeabd978f389eaa62923dfe47530223b6ac600
dianpriatna <dian.priatn@gmail.com>
2026-07-12 23:20:01 +0700

    feat: add Student type definition

---
+++ src/lib/types/student.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} Student
+ *
+ * @property {string} id
+ * @property {string} nis
+ * @property {string} name
+ * @property {string} email
+ * @property {boolean} active
+ */
