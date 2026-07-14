3be9de0ce0f406ec67d09ed0463fb7f5d0f0b785
dianpriatna <dian.priatn@gmail.com>
2026-07-14 18:56:08 +0700

    refactor: make drawer reusable with navigation model

--- src/lib/components/layout/AppLayout.svelte
+++ src/lib/components/layout/AppLayout.svelte
@@ -1,21 +1,5 @@
 <script>
-	import Navbar from './Navbar.svelte';
-	import Drawer from './Drawer.svelte';
-	import Footer from './Footer.svelte';
-
-	let { children } = $props();
+	import { appMenu } from '$lib/navigation/app-menu';
 </script>
 
-<div class="flex min-h-screen flex-col">
-	<Navbar />
-
-	<div class="flex flex-1">
-		<Drawer />
-
-		<main class="flex-1 p-6">
-			{@render children()}
-		</main>
-	</div>
-
-	<Footer />
-</div>
+<Drawer items={appMenu} />
---
+++ src/lib/navigation/app-menu.js
@@ -0,0 +1,26 @@
+export const appMenu = [
+	{
+		id: 'dashboard',
+		label: 'Dashboard',
+		href: '/dashboard',
+		icon: '🏠'
+	},
+	{
+		id: 'exams',
+		label: 'Exams',
+		href: '/dashboard',
+		icon: '📝'
+	},
+	{
+		id: 'profile',
+		label: 'Profile',
+		href: '/profile',
+		icon: '👤'
+	},
+	{
+		id: 'logout',
+		label: 'Logout',
+		href: '/logout',
+		icon: '🚪'
+	}
+];
