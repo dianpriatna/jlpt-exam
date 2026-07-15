fa20c01ccda8a363bfd87dd9d97ac12d6f6982df
dianpriatna <dian.priatn@gmail.com>
2026-07-15 11:34:01 +0700

    error

--- src/routes/exam/[id]/+page.svelte
+++ src/routes/exam/[id]/+page.svelte
@@ -4,8 +4,12 @@
 	const examId = $derived(page.params.id);
 </script>
 
+<svelte:head>
+	<title>Exam {examId}</title>
+</svelte:head>
+
 <h1 class="text-2xl font-bold">
 	Exam {examId}
 </h1>
 
-<p class="mt-2 text-base-content/70">Exam page is ready.</p>
+<p class="text-base-content/70">Sprint 9.1 - Exam Route Ready</p>
